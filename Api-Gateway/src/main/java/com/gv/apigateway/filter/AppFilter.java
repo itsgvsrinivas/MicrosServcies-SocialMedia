858a7d8d5408f47b64310347d1ae9f5084b03ee5
gv <srinivas.srinivas@maybank.com>
2025-03-02 15:25:58 +0800

    Added API Gateway

---
+++ Api-Gateway/src/main/java/com/gv/apigateway/filter/AppFilter.java
@@ -0,0 +1,38 @@
+package com.gv.apigateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Set;
+
+@Component
+@Slf4j
+public class AppFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("[filter]");
+
+        //Validate the requests
+        ServerHttpRequest serverHttpRequest = exchange.getRequest();
+        HttpHeaders httpHeaders = serverHttpRequest.getHeaders();
+        Set keySet = httpHeaders.keySet();
+
+        /*if (!keySet.contains("authKey")) {
+            throw new RuntimeException("Invalid Request- Missing secret key");
+        }
+
+        List<String> secretList = httpHeaders.get("authKey");
+        if (!secretList.get(0).equals("ashokIt@123")) {
+            throw new RuntimeException("Invalid secret key");
+        }*/
+
+        return chain.filter(exchange);
+    }
+}
